58d3f39e1834552a21e67b9f0dd19bfefeb87710
felipeangeli <felipe.angeli@gmail.com>
2025-02-19 17:11:21 -0300

    repositorys

---
+++ src/main/java/com/felipe/aprendendo_spring/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.felipe.aprendendo_spring.infrastructure.repository;
+
+import com.felipe.aprendendo_spring.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
